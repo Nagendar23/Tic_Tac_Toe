5ec2615da821b705c533ac3b44e91da79d734801
Nagendar23 <varkalanagendar777@gmail.com>
2024-09-20 20:10:39 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Board from './TicTacToeGame/Board'
+
+export default function App() {
+  return (
+    <>
+      <div className="App">
+        <Board/>
+      </div>
+    </>
+  )
+}
